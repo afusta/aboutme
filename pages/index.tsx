69b95b70b19e7f327bf51280519602238b6076c2
PierreHy <p.houy@expressivee.com>
2023-07-01 22:06:34 +0200

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,134 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pierre Houy</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <section id="hi" className={`${styles.section1bgimage}`}>
+          <div className={`${styles.section1shadowbg} ${styles.container}`}>
+            {/* <h1
+              className={styles.title}
+              style={{ textAlign: "left", fontSize: "150px", lineHeight: 0.8 }}
+            >
+              A<span className={styles.shadowspan}>mazing</span>
+              <br />
+              fu<span className={styles.shadowspan}>ll</span>
+              <br />
+              sta<span className={styles.shadowspan}>ck</span>
+            </h1> */}
+
+            <div style={{ width: "100%", padding: "0 20% 0 20%" }}>
+              <p
+                style={{
+                  color: "white",
+                  fontSize: "100px",
+                  lineHeight: 0.8,
+                  fontWeight: "bolder",
+                }}
+              >
+                Hello I'm Pierre, I'm a Full Stack engineer
+              </p>
+            </div>
+            {/* <Image
+              src="/images/LogoEpita.png"
+              width={400}
+              height={270}
+              alt="MOI"
+            /> */}
+          </div>
+        </section>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        </a>
+      </footer>
+
+      {/* <style jsx>{`
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style> */}
+    </div>
+  );
+}
